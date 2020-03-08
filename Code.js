d2bca9feb8bc8809fcf43490d0d4d2b371a3b273
Tri Vo <tri.minh.vo@mgm-tp.com>
2020-03-08 15:37:29 +0700

    Format file Code

--- Code.js
+++ Code.js
@@ -3,39 +3,38 @@ var url = "https://docs.google.com/spreadsheets/d/13M6nrNo1EJ0AL1DEK7mTomcVBHeWQ
 function doGet(e) {
   var ss = SpreadsheetApp.openByUrl(url);
   var ws = ss.getSheetByName("Options");
-  var list = ws.getRange(1,1,ws.getRange("A1").getDataRegion().getLastRow(),1).getValues();
-  
-  var tmp= HtmlService.createTemplateFromFile("page");
+  var list = ws.getRange(1, 1, ws.getRange("A1").getDataRegion().getLastRow(), 1).getValues();
+
+  var tmp = HtmlService.createTemplateFromFile("page");
   tmp.list = list;
   return tmp.evaluate();
 }
 
-function userClicked(userInfo){
-   
-   var ss = SpreadsheetApp.openByUrl(url);
-   var ws = ss.getSheetByName("Data");
-   
-   ws.appendRow([userInfo.username,userInfo.age,userInfo.city,userInfo.app]); 
-    
-   Logger.log(userInfo);
+function userClicked(userInfo) {
+
+  var ss = SpreadsheetApp.openByUrl(url);
+  var ws = ss.getSheetByName("Data");
+
+  ws.appendRow([userInfo.username, userInfo.age, userInfo.city, userInfo.app]);
+
+  Logger.log(userInfo);
 }
 
-function include(fileName){
+function include(fileName) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
 }
-function getUserKYK(kyk){
+function getUserKYK(kyk) {
   var ss = SpreadsheetApp.openByUrl(url);
   var ws = ss.getSheetByName("KYK");
-  var userList = ws.getRange(1, 1, ws.getLastRow(),2).getValues();
-  
-  var kykList = userList.map(function (r) { return r[0]; });
-  var butList = userList.map(function (r) { return r[1]; });
-  
+  var userList = ws.getRange(1, 1, ws.getLastRow(), 2).getValues();
+
+  var kykList = userList.map(r => r[0]);
+  var butList = userList.map(r => r[1]);
+
   var pos = kyklist.indexOf(kyk);
-  if(pos > -1){
+  if (pos > -1) {
     return butList[pos];
   } else {
     return 'Undefine';
-    
   }
 }
